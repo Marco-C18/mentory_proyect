40c88850ba14fded06eb45f3c802998299bf41ca
marco <zmarco.20.ac@gmail.com>
2025-10-07 22:43:16 -0500

    Creación de proyecto

---
+++ src/main/java/com/mentory/mentory_proyect/MentoryProyectApplication.java
@@ -0,0 +1,13 @@
+package com.mentory.mentory_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoryProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoryProyectApplication.class, args);
+	}
+
+}
